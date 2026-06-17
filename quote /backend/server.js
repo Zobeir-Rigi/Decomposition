207b0b94e49493a4a16d6e8a71fe377e5772063c
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-06-17 23:41:47 +0100

    Fix GET response to use JSON and match API design

--- quote /backend/server.js
+++ quote /backend/server.js
@@ -14,15 +14,40 @@ const quotes = [
   },
 ];
 
-function pickRandomQuote() {
+function randomQuote() {
   const index = Math.floor(Math.random() * quotes.length);
   return quotes[index];
 }
 
 app.get("/", (req, res) => {
-  console.error("Received a request for a quote, so check your browser for the quote.");
-  const quote = pickRandomQuote();
-  res.send(`"${quote.quote}" -${quote.author}`);
+  const quote = randomQuote();
+  res.json(quote);
+});
+
+app.post("/", (req, res) => {
+  const bodyBytes = [];
+  req.on("data", chunk => bodyBytes.push(...chunk));
+  req.on("end", () => {
+    const bodyString = String.fromCharCode(...bodyBytes);
+    let body;
+    try {
+      body = JSON.parse(bodyString);
+    } catch (error) {
+      console.error(`Failed to parse body ${bodyString} as JSON: ${error}`);
+      res.status(400).send("Expected body to be JSON.");
+      return;
+    }
+    if (typeof body != "object" || !("quote" in body) || !("author" in body)) {
+      console.error(`Failed to extract quote and author from post body: ${bodyString}`);
+      res.status(400).send("Expected body to be a JSON object containing keys quote and author.");
+      return;
+    }
+    quotes.push({
+      quote: body.quote,
+      author: body.author,
+    });
+    res.send("ok");
+  });
 });
 
 app.listen(port, () => {
