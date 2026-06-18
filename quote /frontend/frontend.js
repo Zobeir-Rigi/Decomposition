271c56a17748bb58a0ea21a30300bbef509f8015
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-06-18 12:08:21 +0100

    frontend fetched data from backend

---
+++ quote /frontend/frontend.js
@@ -0,0 +1,19 @@
+const fetchQuote = async () => {
+    const response = await fetch("http://127.0.0.1:3000/")
+    const data = await response.json()
+    // console.log("data is : ", data);
+    // console.log(`"${data.quote}" - ${data.author}`);
+    const q = document.getElementById("quote");
+    q.textContent = data.quote
+
+    const a = document.getElementById("author");
+    a.textContent = data.author;
+};
+
+const button = document.getElementById("new-quote");
+button.addEventListener("click", () => {
+    fetchQuote();
+})
+
+
+// console.log("the function is ", fetchQuote())
